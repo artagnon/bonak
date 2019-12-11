1734275197f1ab8991844ba14a4f2f919477f79e
Ramkumar Ramachandra <artagnon@gmail.com>
2019-12-11 16:55:41 +0100

    [valis] Hacking with Hugo

--- notes/valis.v
+++ notes/valis.v
@@ -1,28 +1,61 @@
-# Category Theory in valis:
-# \approx : Equality, up to homotopy equivalence
-# ~> : Morphism always takes ∞-categories as objects
-# \circ : Composition of ∞-categories
+(* Category Theory in valis:
+   \approx : Equality, up to weak homotopy equivalence
+   ~> : Morphism always takes ∞-categories as objects
+   \bullet : Composition of ∞-categories
+ *)
 
-Inductive Carrier {n : Nat} : Set :=
+Set Universe Polymorphism.
+
+Reserved Notation "a ~> b" (at level 80).
+
+Inductive Carrier : nat -> Type :=
+  | CNil : Carrier O
+  | CAppend : forall {n : nat}, Type -> Carrier n -> Carrier (S n).
+
+Inductive Morphism : forall {n : nat}, Carrier n -> Carrier n -> Type :=
+  | MorphS : forall {n : nat} {A B : Carrier n} {A' B' : Carrier (S n)}, (A ~> B) -> (A' ~> B')
+where "a ~> b" := (Morphism a b).
+
+Notation "( a ; b )" := (existT _ a b) (at level 0).
+
+Fixpoint Arity n : Type :=
   match n with
-  | O => Carrier O                                                    (* The base case *)
-  | S n => forall Carrier n, Carrier (S n)                            (* The objects of an n-Category are mapped to a Higher Category *)
+  | 0 => Type
+  | S n' => { A : Arity n' & Mor n' A 0 tt }
+  end
+
+with Sign n (A:Arity n) : Type :=
+  match n, S with
+  | 0, tt => Type
+  | S n', (A;P) => { s : Sign n' A & app n A P s }
+  end
+
+with app n (A:Arity n) (P:Mor n S 0) (s:Sign n A) : Type :=
+  match n, S with
+  | 0, tt => Type
+  | S n', (A;P) =>
+  end
+
+with Mor n (A:Arity n) p (s:Sign p (down n p A)): Type :=
+  match n, S with
+  | 0, CNil => Type
+  | S n', CAppend T S' => forall A:T, make_arity n' S'
   end.
 
-Inductive Morphism {n : Nat} (a b : Carrier n) [~>] : Set :=
-  match n with
-  | O => a -> b -> (a ~> b)
-  | S n => forall (Carrier n ~> Carrier n), (a ~> b)                 (* The morphisms of the n-Category are also mapped *)
-  end.
+
+
+Reserved Notation "f ∙ g" (at level 55).
 
 (* Definition of an n-Category *)
-Inductive Category {n : Nat} :=
-  | Obj : Carrier n
-  | ~> : Obj ~> Obj
-  | Id : forall A : Obj, A ~> A                                       (* Identity morphism *)
-  | . : forall (A B C : Obj), (B ~> C) ~> (A ~> B) ~> (A ~> C).       (* Composition *)
-  | Assoc : forall (A B C : Obj) (f : C ~> A) (g : B ~> C) (h : A ~> B), (f . g) . h = f . (g . h)
-  | IdLaw : forall A B (f : A ~> B), (Id B . f = f) and (f . Id A = f).
+Record Category {n : nat} : Type :=
+  {
+    Obj : Carrier n
+  ; Id : Obj ~> Obj
+  ; Comp : forall {A B C}, (B ~> C) -> (A ~> B) -> (A ~> C)
+    where "f ∙ g" := (Comp f g)
+  ; Assoc : forall {A B C} (f : C ~> A) (g : B ~> C) (h : A ~> B), (f ∙ g) ∙ h = f ∙ (g ∙ h)
+  ; IdLaw : forall A B (f : A ~> B), (Id B ∙ f = f) /\ (f ∙ Id A = f)
+  }.
 
 Type Functor : Category 2.
 Type NaturalTrans : Category 3.
