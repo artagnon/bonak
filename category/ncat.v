3ebc819c276eefe9f21bd4998bea67571bb33277
Ramkumar Ramachandra <artagnon@gmail.com>
2020-01-14 19:56:10 +0100

    [ncat] Another session with Hugo

--- category/ncat.v
+++ category/ncat.v
@@ -4,11 +4,9 @@
    ∘ : Composition of ∞-categories
  *)
 
-Set Universe Polymorphism.
+Require Import HoTT.
 
-Notation "( a ; b )" := (existT _ a b) (at level 0).
-Notation "'dfst' a" := (projT1 a) (at level 10).
-Notation "'dsnd' a" := (projT2 a) (at level 10).
+Set Universe Polymorphism.
 
 Record NCat :=
 mkNCat {
@@ -18,7 +16,7 @@ mkNCat {
 
 Fixpoint CatOp n : NCat :=
   match n with
-  | O => {| Arity := unit ; Sign := (fun _ => unit) |}
+  | O => {| Arity := Unit ; Sign := (fun _ => Unit) |}
   | S n' => {| Arity := { A: (CatOp n').(Arity) &
                             (CatOp n').(Sign) A -> Type };
               Sign := (fun '(A ; M) =>
@@ -27,10 +25,35 @@ Fixpoint CatOp n : NCat :=
 
 Reserved Notation "f ∘ g" (at level 55).
 
-Inductive Monoid isa Category 0 [rename ~> to \times] :=
-  | Unit : Set -> Monoid.
-  | Multiplication : forall {M : Monoid}, M x M -> M
-  | Munit : forall {M : Monoid}, (Unit x M -> M) and (M x Unit -> M).
+Reserved Notation "f × g" (at level 52).
+
+Definition Monoid := { C : Category 1 | exists A : Ob C, forall B : Ob C, A = B }.
+
+Record Monoid' :=
+  { dom : Type;
+    Unit : dom; }
+    Multiplication : dom -> dom -> dom
+    where "A × B" := (Multiplication A B);
+    MunitL : forall x, Multiplication Unit × x = x;
+    MunitR : forall x, Multiplication x × Unit = x;
+    Massoc : forall x y z, x × (y × z) = (x × y) × z }.
+
+Record Equivalence A B := {
+   f : A -> B;
+   g : B -> A;
+   Idgf : forall x, g (f x) = x;
+   Idfg : forall y, f (g y) = y;
+   Coh : forall x, Idfg (f x) = f_equal f (Idgf x)
+}
+
+Lemma : Equivalence Monoid Monoid'.
+Proof.
+
+
+Inductive Monoid isa Category 1 [rename ~> to \times] :=
+  { Unit : Set -> Monoid.
+    Multiplication : forall {M : Monoid}, M x M -> M
+    Munit : forall {M : Monoid}, (Unit x M -> M) and (M x Unit -> M).
 
 Inductive Group isa Monoid := forall A, A -> A. (* Inverse *)
 Inductive AbelianGroup isa Group := forall {F G}, F + G = G + F.
