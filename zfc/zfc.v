06476b79b843f6a864f53198ffee46f970f92488
Ramkumar Ramachandra <artagnon@gmail.com>
2020-01-28 11:01:24 +0100

    [zfc] With union, intersect; broken proofs

--- zfc/zfc.v
+++ zfc/zfc.v
@@ -1,15 +1,43 @@
 From Coq Require Import ssreflect.
 From Coq.Classes Require Import RelationClasses.
+From Coq Require Import Setoid.
 
 Parameter Obj : Set.
-Axiom LEM : forall (P : Prop), P \/ ~P.
+Parameter φ : Obj.
 Parameter In : Obj -> Obj -> Prop.
+Parameter Union : Obj -> Obj -> Obj.
+Parameter Intersect : Obj -> Obj -> Obj.
 Notation "x ∈ A" := (In x A) (at level 0).
+Notation "x ∉ A" := (~(In x A)) (at level 0).
+Notation "A ∪ B" := (Union A B) (at level 0).
+Notation "A ∩ B" := (Intersect A B) (at level 0).
+Axiom LEM : forall (P : Prop), P \/ ~P.
 Axiom eq : forall (A B : Obj), (A = B) <-> (forall x, x ∈ A <-> x ∈ B).
+Axiom phi : forall x : Obj, x ∉ φ.
+Axiom union : forall A B x : Obj, x ∈ (A ∪ B) <-> x ∈ A \/ x ∈ B.
+Axiom intersect : forall A B x : Obj, x ∈ (A ∩ B) <-> x ∈ A /\ x ∈ B.
 
-Lemma eq_reflexive : forall (A : Obj), A = A.
+Lemma implication_swap : forall A B, (A <-> B) <-> (B <-> A).
+Proof.
+  by intuition.
+Qed.
+
+Theorem eq_reflexive : forall (A : Obj), A = A.
 Proof.
   intros.
   apply eq.
   by reflexivity.
 Qed.
+
+Theorem eq_symmetric : forall (A B : Obj), A = B <-> B = A.
+Proof.
+  intros.
+  rewrite eq.
+Abort.
+
+Theorem eq_transitive : forall (A B C : Obj), A = B /\ B = C -> A = C.
+Proof.
+  intros.
+  rewrite eq.
+Abort.
+
