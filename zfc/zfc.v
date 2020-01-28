019499b0da6f71e8ba2924fdf24e48062aa93e65
Ramkumar Ramachandra <artagnon@gmail.com>
2020-01-28 11:32:03 +0100

    [zfc] Exercise3 with proofs

--- zfc/zfc.v
+++ zfc/zfc.v
@@ -17,27 +17,36 @@ Axiom phi : forall x : Obj, x ∉ φ.
 Axiom union : forall A B x : Obj, x ∈ (A ∪ B) <-> x ∈ A \/ x ∈ B.
 Axiom intersect : forall A B x : Obj, x ∈ (A ∩ B) <-> x ∈ A /\ x ∈ B.
 
-Lemma implication_swap : forall A B, (A <-> B) <-> (B <-> A).
-Proof.
-  by intuition.
-Qed.
+Section Exercise1.
+  Theorem eq_reflexive : forall (A : Obj), A = A.
+  Proof.
+    by move => A; apply eq => //=.
+  Qed.
 
-Theorem eq_reflexive : forall (A : Obj), A = A.
-Proof.
-  intros.
-  apply eq.
-  by reflexivity.
-Qed.
+  Theorem eq_symmetric : forall (A B : Obj), A = B <-> B = A.
+  Proof.
+    move => A B; rewrite eq.
+  Abort.
 
-Theorem eq_symmetric : forall (A B : Obj), A = B <-> B = A.
-Proof.
-  intros.
-  rewrite eq.
-Abort.
+  Theorem eq_transitive : forall (A B C : Obj), A = B /\ B = C -> A = C.
+  Proof.
+    move => A B C; rewrite eq.
+  Abort.
+End Exercise1.
 
-Theorem eq_transitive : forall (A B C : Obj), A = B /\ B = C -> A = C.
-Proof.
-  intros.
-  rewrite eq.
-Abort.
+Section Exercise2.
+End Exercise2.
 
+Section Exercise3.
+  Theorem union_contract : forall A : Obj, A ∪ A = A.
+  Proof.
+    move => A; apply eq => x; rewrite union.
+    by intuition.
+  Qed.
+
+  Theorem union_phi : forall A : Obj, A ∪ φ = A.
+  Proof.
+    move => A; apply eq => x; rewrite union.
+    by intuition; apply phi in H0 => //=.
+  Qed.
+End Exercise3.
