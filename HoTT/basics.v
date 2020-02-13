446cb109dd8c1727f2c29445785d925d03ac0334
Ramkumar Ramachandra <artagnon@gmail.com>
2020-02-13 17:50:48 +0100

    [basics] Introduce Peano naturals

--- HoTT/basics.v
+++ HoTT/basics.v
@@ -8,16 +8,26 @@ Section Basics.
   Theorem TrueDoesNotImplyFalse : True -> False.
   Proof.
     (* Due to enforced exhaustive pattern-matching on inhabitants of the LHS,
-    * we'd need to match up an inhabitant of True (there is only I)
-    * to some inhabitant of False, but there are no inhabitants of False.
-    *)
+     * we'd need to match up an inhabitant of True (there is only I)
+     * to some inhabitant of False, but there are no inhabitants of False.
+     *)
   Abort.
+
+  (* Any inhabitated type implies unit *)
   Theorem f : nat -> unit.
   Proof.
     intros x.
     exact tt.
   Qed.
 
+  (* The Curry-Howard correspondence applies to f *)
   Definition f' : nat -> unit := fun _ => tt.
-  Definition trivial : ~True -> False := fun (k : ~True) => k I.
-  End Basics.
+
+  (* This is equivalent to (True -> False) -> False.
+   * As both True -> False and False are equally false, this is provable.
+   *)
+  Definition Equiconsistent_with_empty : ~True -> False := fun (k : ~True) => k I.
+
+  (* Peano naturals *)
+  Inductive nat : Set := O : nat | S : nat -> nat.
+End Basics.
