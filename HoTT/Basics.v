ff06efda801bbc19c2b7652ad0b7ff36921d0f0b
Ramkumar Ramachandra <artagnon@gmail.com>
2020-02-15 16:03:26 +0100

    [HoTT/Basics] Housekeeping

--- HoTT/Basics.v
+++ HoTT/Basics.v
@@ -1,33 +1,33 @@
-Section Basics.
+From Coq Require Import ssreflect.
+
+Section Logic.
   Theorem FalseImpliesAnything : forall A, False -> A.
   Proof.
-    intros A.
-    exact (fun x : False => match x with end).
+    by intros A; exact (fun x : False => match x with end).
   Qed.
 
   Theorem TrueDoesNotImplyFalse : True -> False.
   Proof.
     (* Due to enforced exhaustive pattern-matching on inhabitants of the LHS,
      * we'd need to match up an inhabitant of True (there is only I)
-     * to some inhabitant of False, but there are no inhabitants of False.
-     *)
+     * to some inhabitant of False, but there are no inhabitants of False. *)
   Abort.
 
-  (* Any inhabitated type implies unit *)
+  (* Any inhabitated type implies unit. *)
   Theorem f : nat -> unit.
   Proof.
-    intros x.
-    exact tt.
+    by intros x; exact tt.
   Qed.
 
   (* The Curry-Howard correspondence applies to f *)
   Definition f' : nat -> unit := fun _ => tt.
 
   (* This is equivalent to (True -> False) -> False.
-   * As both True -> False and False are equally false, this is provable.
-   *)
+   * As both True -> False and False are equally false, this is provable. *)
   Definition Equiconsistent_with_empty : ~True -> False := fun (k : ~True) => k I.
+End Logic.
 
+Section Inductives.
   (* Peano naturals *)
   Inductive nat : Set := O : nat | S : nat -> nat.
-End Basics.
+End Inductives.
