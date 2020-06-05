341d6bbe5f13c7c97aaaea70f084c86a5ae0df65
Ramkumar Ramachandra <artagnon@gmail.com>
2020-06-05 15:50:04 +0200

    [Cubical] Changes to cube, subcube

--- theories/HoTT/Cubical.v
+++ theories/HoTT/Cubical.v
@@ -14,10 +14,10 @@ Record Cubical (n : nat) :=
   hd {n'} {Hn' : n' <= n} : csp Hn' -> csp (LP Hn') ;
   box {n' p} {Hn' : n' <= n} (Hp : p <= n) : csp Hn' -> Type@{l} ;
   layer {n' p} {Hn' : n' <= n} {Hp : p < n} : forall {D : csp Hn'}, box Hp D -> Type@{l} ;
-  cube {n' p} {Hn' : n' <= n} {Hp : p <= n} : forall {D : csp Hn'}, box Hp D -> Type@{l} -> box Hp D -> Type@{l} ;
+  cube {n' p} {Hn' : n' <= n} {Hp : p <= n} : forall {D : csp Hn'}, (box Hn' D -> Type@{l}) -> box Hp D -> Type@{l} ;
   subbox {n' p q} {Hn' : n' <= n} {Hp : p <= n} (Hq : q < n) : forall {D : csp Hn'}, box Hp D -> box Hp (hd D) ;
   sublayer {n' p q} {Hn' : n' <= n} {Hp : p <= n} (Hq : q < n) : forall {D : csp Hn'} (d : box Hp D), layer d -> layer (subbox Hq d) ;
-  subcube {n' p q} {Hn' : n' <= n} {Hp : p <= n} (Hq : q < n) : forall {D : csp Hn'}, (box Hp D -> Type@{l}) -> box Hp D ;
+  subcube {n' p q} {Hn' : n' <= n} {Hp : p <= n} (Hq : q < n) : forall {D : csp Hn'} (E : box Hp D -> Type@{l}) (d : box Hp D) (b : cube E d), (box Hp D -> Type@{l}) -> box Hp D ;
   cohbox {n' p q r} {Hn' : n' <= n} {Hp : p <= n} (Hq : q < n) (Hr : r < n) : forall {D : csp Hn'}, box Hp D ;
   cohlayer {n' p q r} {Hn' : n' <= n} {Hp : p < n} (Hq : q < n) (Hr : r < n) : forall {D : csp Hn'}, box Hp D -> Type@{l} ;
   cohcube {n' p q r} {Hn' : n' <= n} {Hp : p <= n} (Hq : q < n) (Hr : r < n) : forall {D : csp Hn'}, (box Hp D -> Type@{l}) -> Type@{l}
