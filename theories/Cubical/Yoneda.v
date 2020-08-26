66c395b5974528852042c2518667262ebf5ed99e
Ramkumar Ramachandra <artagnon@gmail.com>
2020-08-26 16:04:20 +0200

    [Yoneda] Fix broken proofs

--- theories/Cubical/Yoneda.v
+++ theories/Cubical/Yoneda.v
@@ -11,20 +11,28 @@ Definition le_refl {n} : n <= n :=
 Definition le_trans {n m p} (Hnm : n <= m) (Hmp : m <= p) : n <= p :=
   fun q (Hqn : Peano.le q n) => Hmp _ (Hnm _ Hqn).
 
+Infix "↕" := le_trans (at level 30).
+
 Theorem le_S_up {n m} (Hnm : n <= m) : n <= S m.
   unfold le.
   intros p Hpn.
   rewrite Hpn.
   apply (Peano.le_S n m).
-  admit.
-Admitted.
+  apply Hnm.
+  reflexivity.
+Defined.
+
+Notation "↑ h" := (le_S_up h) (at level 40).
 
 Theorem le_S_down {n m} (Hnm : S n <= m) : n <= m.
   unfold le.
   intros p Hpn.
-  rewrite Hpn.
-  admit.
-Admitted.
+  apply Hnm.
+  apply Peano.le_S.
+  auto.
+Defined.
+
+Notation "⇓ p" := (le_S_down p) (at level 40).
 
 Theorem le_trans_assoc {n m p q} (Hnm : n <= m) (Hmp : m <= p) (Hpq : p <= q) :
   Hnm ↕ (Hmp ↕ Hpq) = (Hnm ↕ Hmp) ↕ Hpq.
