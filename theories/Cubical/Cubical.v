75ef1f9615873c5d4c669f2e947571e4276fe657
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-09-28 17:00:45 +0200

    extended version with tactics

--- theories/Cubical/Cubical.v
+++ theories/Cubical/Cubical.v
@@ -61,7 +61,7 @@ Record Cubical {n : nat} :=
 Notation "l '.1'" := (projT1 l) (at level 40).
 Notation "l '.2'" := (projT2 l) (at level 40).
 
-Theorem le_refl_irrelevance : forall n (H:n<=n), H = le_refl n.
+Theorem le_irrelevance : forall {n m} (H H':n<=m), H = H'.
 Admitted.
 
 Theorem thm1 : forall {n} (H:n <= S n), le_dec H = right (le_refl n).
@@ -70,8 +70,22 @@ intros.
 destruct (le_dec H) as [Heq|].
 - exfalso. apply n_Sn in Heq as [].
 - f_equal.
-  apply le_refl_irrelevance.
+  apply le_irrelevance.
 Defined.
+
+Theorem le_disjoint : forall n m, S n <= m -> m <= n -> False.
+Admitted.
+
+Theorem thm2 : forall {n n'} (H:n' <= S n) (H':n' <= n), le_dec H = right H'.
+Proof.
+intros.
+destruct (le_dec H) as [->|].
+- exfalso.
+  now apply le_disjoint in H'.
+- f_equal.
+  apply le_irrelevance.
+Defined.
+
 
 Fixpoint cubical {n : nat} : Cubical (n:=n).
 Proof.
@@ -101,8 +115,9 @@ unshelve econstructor.
     * injection Heq as ->.
       rewrite (thm1 (⇓ Hn')).
       exact (D.1). (* hd *)
-    *
-Defined.
-end.
+    * rewrite (thm2 (⇓ Hn') (le_trans (le_S_up (le_refl _)) Hineq)).
+      now apply cn.(hd).
+  + intros n' Hn' D. simpl in *.
+  
 
 End Cubical.
