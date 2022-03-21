714613f41317bf4b79dbc95dc31bade8b00af934
Ramkumar Ramachandra <r@artagnon.com>
2022-03-21 12:42:20 +0100

    Yoneda: sprinkle in some comments

--- theories/Cubical/Yoneda.v
+++ theories/Cubical/Yoneda.v
@@ -25,12 +25,15 @@ Theorem le_irrelevance : forall {n m} (H H' : n <= m), H =S H'.
   reflexivity.
 Qed.
 
+(* False in SProp has no inhabitant *)
 Inductive SFalse : SProp :=.
 
+(* A simple contraction used in the next lemma *)
 Lemma le'_1_O_contra: le' 1 O -> SFalse.
   inversion 1.
 Qed.
 
+(* Contradiction of type n.+1 <= 0 *)
 Theorem le_contra {n}: n.+1 <= O -> False.
   intros; elimtype SFalse; unfold le in H.
   specialize H with (p := 1); apply le'_1_O_contra.
@@ -61,15 +64,20 @@ Lemma le_implies_leYoneda {n p} : Peano.le p n -> p <= n.
   apply le_implies_le'. now lia.
 Qed.
 
+(* Reflexivity in leYoneda *)
 Definition le_refl n : n <= n :=
   fun _ x => x. (* Coq bug! *)
 
 Notation "¹" := le_refl (at level 0).
 
+
+(* Transitivity in leYoneda *)
 Definition le_trans {n m p} (Hnm : n <= m) (Hmp : m <= p) : n <= p :=
   fun q (Hqn : le' q n) => Hmp _ (Hnm _ Hqn).
 
 Infix "↕" := le_trans (at level 45).
+
+(* Some trivial inequality proofs in leYoneda *)
 
 Theorem le_S_up {n m} (Hnm : n <= m) : n <= m.+1.
   unfold le; intros p Hpn.
@@ -97,6 +105,9 @@ Qed.
 
 Notation "⇑ p" := (raise_S_both p) (at level 40).
 
+(* A tactic to turn inequalities of the form p.+2 <= q.+1 into p.+2 <= q.+2;
+ * find_raise is a helper for the tactic *)
+
 Ltac find_raise q :=
   match q with
   | ?q.+1 => find_raise q
@@ -121,7 +132,8 @@ Lemma leYoneda_implies_Le {n p}: p <= n -> p <~ n.
   intros H. apply leYoneda_implies_le in H.
 Admitted.
 
-(* A couple of morphisms *)
+(* A couple of properties of the two connections, asserting the equality
+ * of morphisms *)
 
 Lemma Le_refl_morphism n: leYoneda_implies_Le (¹ n) = Le_refl' _.
 Admitted.
@@ -131,11 +143,11 @@ Lemma Le_S_morphism {n p} (Hp: p.+1 <= n.+1):
 Admitted.
 
 (* Another way to state leYoneda *)
-
 Inductive Leind n : forall p, p <= n -> Type :=
 | Leind_n : Leind n n (¹ n)
 | Leind_S p Hp : Leind n p.+1 Hp -> Leind n p (↓ Hp).
 
+(* A constructor of Leind *)
 Lemma Leind_construct {n p} Hp: Leind n p Hp.
 intros; induction (leYoneda_implies_Le Hp).
 - rewrite (le_irrelevance Hp (¹ n)). (* should not be needed *)
@@ -143,6 +155,8 @@ intros; induction (leYoneda_implies_Le Hp).
 - rewrite (le_irrelevance Hp (↓ (Le_implies_leYoneda l))).
   apply Leind_S, IHl.
 Defined.
+
+(* le_induction along with a couple of computational properties *)
 
 Theorem le_induction {n p} (Hp : p <= n) (P: forall p (Hp: p <= n), Type)
   (H_base: P n (¹ n))
@@ -166,7 +180,7 @@ Proof.
   rewrite Le_S_morphism; simpl. now destruct le_irrelevance.
 Qed.
 
-(* Some helpers *)
+(* Some helper-abbreviations *)
 
 Definition le_induction' {n p} (Hp: p.+1 <= n.+1)
   (P: forall p (Hp: p.+1 <= n.+1), Type)
