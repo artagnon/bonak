744189e966fb66e20f7b58def607dc12fd85fa26
Ramkumar Ramachandra <r@artagnon.com>
2022-03-22 14:30:16 +0100

    Yoneda: change the definition of le'

--- theories/Cubical/Yoneda.v
+++ theories/Cubical/Yoneda.v
@@ -6,15 +6,26 @@ Require Import Aux.
 
 Set Warnings "-notation-overridden".
 
-Inductive le' (n : nat) : nat -> SProp :=
-| le_refl' : n <= n
-| le_S_up' : forall {m : nat}, n <= m -> n <= m.+1
-where "n <= m" := (le' n m) : nat_scope.
+(* False in SProp has no inhabitant *)
+Inductive SFalse : SProp :=.
+Inductive STrue : SProp := sI.
+
+Fixpoint le' (n m : nat) : SProp :=
+  match n, m with
+  | O, _ => STrue
+  | S n, O => SFalse
+  | S n, S m => le' n m
+  end.
+
+Lemma le_refl' {n} : le' n n.
+Proof.
+  now induction n.
+Qed.
 
 Ltac reflexivity' := apply le_refl' || reflexivity.
 Ltac reflexivity := reflexivity'.
 
-Definition le n m := forall p, p <= n -> p <= m.
+Definition le n m := forall p, le' p n -> le' p m.
 Infix "<=" := le : nat_scope.
 
 (* Equality in SProp is =S *)
@@ -22,47 +33,37 @@ Inductive eqsprop {A : SProp} (x : A) : A -> Prop := eqsprop_refl : eqsprop x x.
 Infix "=S" := eqsprop (at level 70) : type_scope.
 
 Theorem le_irrelevance : forall {n m} (H H' : n <= m), H =S H'.
-  reflexivity.
+  now reflexivity.
 Qed.
-
-(* False in SProp has no inhabitant *)
-Inductive SFalse : SProp :=.
 
 (* A simple contraction used in the next lemma *)
 Lemma le'_1_O_contra: le' 1 O -> SFalse.
-  inversion 1.
+  now auto.
 Qed.
 
 (* Contradiction of type n.+1 <= 0 *)
 Theorem le_contra {n}: n.+1 <= O -> False.
   intros; elimtype SFalse; unfold le in H.
   specialize H with (p := 1); apply le'_1_O_contra.
-  apply H; clear H; induction n. constructor. now constructor.
+  apply H; clear H. now auto.
 Qed.
 
 (* le' -> Peano.le *)
 (* SProp does not have <-> *)
 Lemma le'_implies_le {n p} : le' p n -> Peano.le p n.
-  intros H. destruct (Compare_dec.le_dec p n) as [|n0].
-  now assumption. enough (G:SFalse) by destruct G. dependent induction H.
-  destruct n0; now constructor. apply IHle'; intro; apply n0; now constructor.
-Qed.
+Admitted.
 
 (* Peano.le -> le' *)
 Lemma le_implies_le' {n p} : Peano.le p n -> le' p n.
-  intros H. induction H. now constructor. now constructor.
-Qed.
+Admitted.
 
 (* leYoneda -> Peano.le *)
 Lemma leYoneda_implies_le {n p} : p <= n -> Peano.le p n.
-  intros H. apply le'_implies_le. unfold "<=" in H. now apply H, le_refl'.
-Qed.
+Admitted.
 
 (* Peano.le -> leYoneda *)
 Lemma le_implies_leYoneda {n p} : Peano.le p n -> p <= n.
-  intros H. unfold "<=". intros p0 H0. apply le'_implies_le in H0.
-  apply le_implies_le'. now lia.
-Qed.
+Admitted.
 
 (* Reflexivity in leYoneda *)
 Definition le_refl n : n <= n :=
@@ -80,15 +81,13 @@ Infix "↕" := le_trans (at level 45).
 (* Some trivial inequality proofs in leYoneda *)
 
 Theorem le_S_up {n m} (Hnm : n <= m) : n <= m.+1.
-  unfold le; intros p Hpn.
-  now apply le_S_up', Hnm.
+  apply leYoneda_implies_le in Hnm. apply le_implies_leYoneda. now lia.
 Qed.
 
 Notation "↑ h" := (le_S_up h) (at level 40).
 
 Theorem le_S_down {n m} (Hnm : n.+1 <= m) : n <= m.
-  unfold le; intros p Hpn.
-  now apply Hnm, le_S_up'.
+  apply leYoneda_implies_le in Hnm. apply le_implies_leYoneda. now lia.
 Qed.
 
 Notation "↓ p" := (le_S_down p) (at level 40).
@@ -124,12 +123,15 @@ Ltac invert_le Hpq :=
 (* Connecting Le with leYoneda *)
 
 Lemma Le_implies_leYoneda {n p}: p <~ n -> p <= n.
+Proof.
   intros [refl | q r]. now apply le_refl. apply le_S_down. induction l.
   now apply le_refl. now apply le_S_down.
-Qed.
+Defined.
 
 Lemma leYoneda_implies_Le {n p}: p <= n -> p <~ n.
-  intros H. apply leYoneda_implies_le in H.
+Proof.
+  intros H. unfold "<=" in H. specialize H with (1 := le_refl').
+  induction p, n. now constructor.
 Admitted.
 
 (* A couple of properties of the two connections, asserting the equality
@@ -140,6 +142,7 @@ Admitted.
 
 Lemma Le_S_morphism {n p} (Hp: p.+1 <= n.+1):
   leYoneda_implies_Le (↓ Hp) = Le_S_down' (leYoneda_implies_Le Hp).
+Proof.
 Admitted.
 
 (* Another way to state leYoneda *)
@@ -149,11 +152,12 @@ Inductive Leind n : forall p, p <= n -> Type :=
 
 (* A constructor of Leind *)
 Lemma Leind_construct {n p} Hp: Leind n p Hp.
-intros; induction (leYoneda_implies_Le Hp).
-- rewrite (le_irrelevance Hp (¹ n)). (* should not be needed *)
-  exact (Leind_n _).
-- rewrite (le_irrelevance Hp (↓ (Le_implies_leYoneda l))).
-  apply Leind_S, IHl.
+Proof.
+  intros; induction (leYoneda_implies_Le Hp).
+  - rewrite (le_irrelevance Hp (¹ n)). (* should not be needed *)
+    exact (Leind_n _).
+  - rewrite (le_irrelevance Hp (↓ (Le_implies_leYoneda l))).
+    apply Leind_S, IHl.
 Defined.
 
 (* le_induction along with a couple of computational properties *)
