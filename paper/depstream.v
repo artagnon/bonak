09075a4b58faf193cdd14fc19bf66d95c432437c
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2024-09-15 12:18:06 +0200

    Adding typing of this and next in depstream.v

--- paper/depstream.v
+++ paper/depstream.v
@@ -10,4 +10,7 @@ Context (D:A->Type) (v : forall a, D a -> B a) (s : forall a d, D (f a (v a d)))
 CoFixpoint g a (d:D a) : DepStream a :=
   {| this := v a d; next := g (f a (v a d)) (s a d) |}.
 
+Definition take_this u x : B u := x.(this u).
+Definition take_next u x : DepStream (f u (this u x)) := x.(next u).
+
 End DepStream.
