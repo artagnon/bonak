b9ee2e545ddd94d500a2012d7d75215f1a92a3f9
Ramkumar Ramachandra <r@artagnon.com>
2025-09-08 14:31:43 +0100

    vType: trim final printing

--- theories/νType/νType.v
+++ theories/νType/νType.v
@@ -854,12 +854,10 @@ Definition SemiCubical := νTypes hbool.
 
 (** Some examples *)
 
-Notation "{ x : A && P }" := (sigT (A := A) (fun x => P)): type_scope.
-
-Example SemiSimplicial2 := Eval lazy -[projT2] in
+Example SemiSimplicial2 := Eval lazy in
  (νTypeAt hunit 2).(prefix'' _).
 Print SemiSimplicial2.
 
-Example SemiCubical2 := Eval lazy -[projT2] in
+Example SemiCubical2 := Eval lazy in
  (νTypeAt hbool 2).(prefix'' _).
 Print SemiCubical2.
